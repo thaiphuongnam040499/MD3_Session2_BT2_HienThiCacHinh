7a817d6ab7463143195764d2f1f260c9001aa394
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-09 09:12:20 +0700

    bt2

--- src/HienThiCacLoaiHinh.java
+++ src/HienThiCacLoaiHinh.java
@@ -1,18 +1,47 @@
+import java.util.Scanner;
+
 public class HienThiCacLoaiHinh {
     public static void main(String[] args) {
-        System.out.println("In Hinh Chu Nhat");
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 7; j++) {
-                System.out.print("* ");
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            int choice;
+            System.out.println("1.\tPrint the rectangle\n" +
+                    "2.\tPrint the square triangle (The corner is square at 4 different angles: top-left, top-right, botton-left, botton-right)\n" +
+                    "3.\tPrint isosceles triangle\n" +
+                    "4.\tExit\n");
+            choice = Integer.parseInt(scanner.nextLine());
+            switch (choice){
+                case 1:
+                    for (int i = 1; i <= 3; i++) {
+                        for (int j = 1; j <= 7; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i <= 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i <= 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 4:
+                    System.out.println("thoat chuong trinh");
+                    System.exit(0);
+                default:
+                    System.err.println("Lựa chọn không hợp lệ");
+                    break;
             }
-            System.out.println();
-        }
-        System.out.println("In Hinh Tam Giac");
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
         }
     }
 }
